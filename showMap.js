9ef0dae57245240de29276f9b02dc0c21073717f
shaniOO3 <official.solo003@gmail.com>
2020-09-21 12:05:43 +0530

    first

---
+++ showMap.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import {Platform, SafeAreaView, StyleSheet, View} from 'react-native'
+import MapboxGL from '@react-native-mapbox-gl/maps';
+MapboxGL.setAccessToken(
+    'pk.eyJ1Ijoic29sbzAwMyIsImEiOiJja2Y5dWxuaHIwaGx2MnFweDJscXA2NG94In0.B0VegnM-IbkUiyEYdToSuw',
+);
+const IS_ANDROID=Platform.OS==='android';
+export default class showMap extends Component {
+    async UNSAFE_componentWillMount(){
+        if(IS_ANDROID){
+            const is_Granted=await MapboxGL.requestAndroidLocationPermissions();
+            this.setState({
+                isAndroidPermissionGranted :is_Granted,
+                isFetchingAndroidPermission:false,
+            });
+        }
+    }
+    constructor(props){
+        super(props);
+        this.state={
+            isAndroidPermissionGranted:false,
+            isFetchingAndroidPermission:IS_ANDROID,
+            coordinates: [[76.271080,10.850516]],
+            showUserLocation:true,
+            location:[76.271080,10.850516],
+        };
+    }
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <View style={styles.container}>
+                    <MapboxGL.MapView
+                        ref ={c=>(this._map=c)}
+                        zoomLevel={14}
+                        centerCoordinate={this.state.coordinates[0]}
+                        showUserLocation={true}
+                        style={styles.container}
+                        userTrackingMode={this.state.userSelectedUserTrackingMode}
+                    >
+                        <MapboxGL.Camera
+                            zoomLevel={6}
+                            animationMode={'flyTo'}
+                            animationDuration={0}
+                            ref={c=>(this.camera=c)}
+                            centerCoordinate={this.state.location}
+                        >
+
+                        </MapboxGL.Camera>
+
+                        <MapboxGL.UserLocation>
+
+                        </MapboxGL.UserLocation>
+                    
+
+                    </MapboxGL.MapView>
+                </View>
+            </SafeAreaView>
+        )
+    }
+}
+const styles=StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
