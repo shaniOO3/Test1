31a3aa8dc80cfcb53ad487385a2baa7e2ab95973
shaniOO3 <official.solo003@gmail.com>
2020-10-19 12:56:15 +0530

    new

--- showMap.js
+++ showMap.js
@@ -1,202 +1,132 @@
-import React, { Component } from 'react'
-import {Platform, SafeAreaView, StyleSheet, View} from 'react-native'
+import React, {Component} from 'react';
+import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
 import RNAndroidLocationEnabler from 'react-native-android-location-enabler';
+import GeoFencing from 'react-native-geo-fencing';
+
 
 MapboxGL.setAccessToken(
-    'pk.eyJ1Ijoic29sbzAwMyIsImEiOiJja2Y5dWxuaHIwaGx2MnFweDJscXA2NG94In0.B0VegnM-IbkUiyEYdToSuw',
+  'pk.eyJ1Ijoic29sbzAwMyIsImEiOiJja2Y5dWxuaHIwaGx2MnFweDJscXA2NG94In0.B0VegnM-IbkUiyEYdToSuw',
 );
 
-const IS_ANDROID=Platform.OS==='android';
-
-const getCoordinatesValanchery = async () => {
-    const data = await fetch(
-      "http://mobilemate-001-site6.atempurl.com/api/Location"
-    );
-    const json = await data.json();
-    const CoordinatesValanchery = json.Result[2].Coordinates.map((coordinate) =>
-      coordinate.LatLng.split(",").reverse()
-      .map((x) => +x)
-    );
-    return CoordinatesValanchery;
-  };
-  console.log(getCoordinatesValanchery())
-  
-  
-  ();
+const IS_ANDROID = Platform.OS === 'android';
 
 export default class showMap extends Component {
-
-    async UNSAFE_componentWillMount(){
-        if(IS_ANDROID){
-            this.turnOnLocation()
-            const is_Granted=await MapboxGL.requestAndroidLocationPermissions();
+    async UNSAFE_componentWillMount() {
+        if (IS_ANDROID) {
+            this.turnOnLocation();
+            const is_Granted = await MapboxGL.requestAndroidLocationPermissions();
             this.setState({
-                isAndroidPermissionGranted :is_Granted,
-                isFetchingAndroidPermission:false,
+                isAndroidPermissionGranted: is_Granted,
+                isFetchingAndroidPermission: false,
             });
         }
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            isAndroidPermissionGranted:false,
-            isFetchingAndroidPermission:IS_ANDROID,
-            coordinates: [[76.271080,10.850516]],
-            showUserLocation:true,
-            location:[76.271080,10.850516],
+        this.state = {
+            isAndroidPermissionGranted: false,
+            isFetchingAndroidPermission: IS_ANDROID,
+            coordinates: [[76.27108, 10.850516]],
+            showUserLocation: [75.987450,10.938220],
+            location: [76.27108, 10.850516],
+            polygonCoordinates: [],
         };
     }
 
+    componentDidMount() {
+        fetch('http://mobilemate-001-site6.atempurl.com/api/Location')
+            .then((response) => response.json())
+            .then((json) => {
+                json.Result.forEach((location) => {
+                    const Coordinates = location.Coordinates.map(
+                    (coordinate) => coordinate.LatLng.split(',')
+                        .reverse()
+                        .map((x) => +x),
+                    ); 
+                    this.setState({polygonCoordinates: [...this.state.polygonCoordinates, Coordinates] });
+                });
+            });
 
-    getCoordinatesValanchery().then(()=>{
-        render() {
+        let point = [
+            76.116697,10.907111
+        ];
 
-            return (
-                <SafeAreaView style={styles.container}>
-                    <View style={styles.container}>
-                        <MapboxGL.MapView
-                            ref ={c=>(this._map=c)}
-                            zoomLevel={14}
-                            centerCoordinate={this.state.coordinates[0]}
-                            showUserLocation={true}
-                            style={styles.container}
-                            userTrackingMode={this.state.userSelectedUserTrackingMode}
-                        >
-    
-                            <MapboxGL.Camera
-                                zoomLevel={10}
-                                animationMode={'flyTo'}
-                                animationDuration={0}
-                                ref={c=>(this.camera=c)}
-                                centerCoordinate={this.state.location}
-                            >
-    
-                            </MapboxGL.Camera>
-    
-                            <MapboxGL.UserLocation>
-    
-                            </MapboxGL.UserLocation>
-    
-                            <MapboxGL.ShapeSource 
-                                id={'abcd'}
-    
-                                shape={{
-                                    type: 'Feature',
-                                    properties:{
-                                        icon: 'exampleicon',
-                                    },
-                                    geometry: {
-                                        type: 'Polygon',
-                                        coordinates:[
-    
-                                
-                                            getCoordinatesValanchery()
-    
-                                        ]
-                                    }
-                                }}>
-    
-                                <MapboxGL.LineLayer
-                                    id={'abcd'}
-                                    style={{lineColor:'#0001fd',lineWidth:2}}
-                                >
-    
-                                </MapboxGL.LineLayer>
-    
-                            </MapboxGL.ShapeSource>
-    
-                            
-    
-                        </MapboxGL.MapView>
-                    </View>
-                </SafeAreaView>
-            )
-        }
+        GeoFencing.containsLocation(point,this.state.polygonCoordinates)
+            .then(() => console.log('point is within polygon'))
+            .catch(() => console.log('point is NOT within polygon'))
     }
-    
-    //     return (
-    //         <SafeAreaView style={styles.container}>
-    //             <View style={styles.container}>
-    //                 <MapboxGL.MapView
-    //                     ref ={c=>(this._map=c)}
-    //                     zoomLevel={14}
-    //                     centerCoordinate={this.state.coordinates[0]}
-    //                     showUserLocation={true}
-    //                     style={styles.container}
-    //                     userTrackingMode={this.state.userSelectedUserTrackingMode}
-    //                 >
 
-    //                     <MapboxGL.Camera
-    //                         zoomLevel={10}
-    //                         animationMode={'flyTo'}
-    //                         animationDuration={0}
-    //                         ref={c=>(this.camera=c)}
-    //                         centerCoordinate={this.state.location}
-    //                     >
+    render() {
+        return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.container}>
+                <MapboxGL.MapView
+                    ref={(c) => (this._map = c)}
+                    zoomLevel={14}
+                    centerCoordinate={this.state.coordinates[0]}
+                    showUserLocation={[75.987450,10.938220]}
+                    style={styles.container}
+                    userTrackingMode={this.state.userSelectedUserTrackingMode}>
+                    
+                    <MapboxGL.Camera
+                        zoomLevel={13}
+                        animationMode={'flyTo'}
+                        animationDuration={0}
+                        ref={(c) => (this.camera = c)}
+                        centerCoordinate={this.state.location}
+                    />
 
-    //                     </MapboxGL.Camera>
+                    <MapboxGL.UserLocation
+                        >
 
-    //                     <MapboxGL.UserLocation>
+                    </MapboxGL.UserLocation>
 
-    //                     </MapboxGL.UserLocation>
+                    <MapboxGL.ShapeSource
+                        id={'abcd'}
+                        shape={{
+                            type: 'Feature',
+                            properties: {
+                                icon: 'exampleicon',
+                            },
+                            geometry: {
+                                type: 'MultiPolygon',
+                                coordinates: [this.state.polygonCoordinates],
+                            },
+                    }}>
 
-    //                     <MapboxGL.ShapeSource 
-    //                         id={'abcd'}
+                    <MapboxGL.FillLayer
+                        id={'abcd'}
+                        style={{fillColor: '#FF0000', fillOpacity: 0.6}}
+                    />
 
-    //                         shape={{
-    //                             type: 'Feature',
-    //                             properties:{
-    //                                 icon: 'exampleicon',
-    //                             },
-    //                             geometry: {
-    //                                 type: 'Polygon',
-    //                                 coordinates:[
+                    </MapboxGL.ShapeSource>
 
-                            
-    //                                     getCoordinatesValanchery()
-
-    //                                 ]
-    //                             }
-    //                         }}>
-
-    //                         <MapboxGL.LineLayer
-    //                             id={'abcd'}
-    //                             style={{lineColor:'#0001fd',lineWidth:2}}
-    //                         >
-
-    //                         </MapboxGL.LineLayer>
-
-    //                     </MapboxGL.ShapeSource>
-
-                        
-
-    //                 </MapboxGL.MapView>
-    //             </View>
-    //         </SafeAreaView>
-    //     )
-    // }
+                </MapboxGL.MapView>
+            
+            </View>
+        </SafeAreaView>
+        );
+    }
 
     turnOnLocation = () => {
-        RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({interval: 10000, fastInterval: 5000})
-            .then(data => {
-                if (data === "already-enable") {
-
-                } else {
-                    setTimeout(() => {
-                        
-                    }, 1000)
-                }
-        }).catch(err => {
-            
+        RNAndroidLocationEnabler.promptForEnableLocationIfNeeded({
+            interval: 10000,
+            fastInterval: 5000,
         })
+            .then((data) => {
+                if (data === 'already-enable') {
+                
+                } else {
+                    setTimeout(() => {}, 1000);
+                }
+            })
+            .catch((err) => {});
     };
 }
-
-const styles=StyleSheet.create({
+const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
 });
-
